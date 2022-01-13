0302810abe9b708c60a12a9658266690be47590d
Juun Pak <june.mintlatte@gmail.com>
2022-01-13 13:03:32 +0900

    ✨ Implement to swipe the slide
    Need to fix: sometimes it is not working

--- src/components/slide/MainBanner.jsx
+++ src/components/slide/MainBanner.jsx
@@ -15,6 +15,7 @@ const MainBanner = () => {
   const [centerMode, setCenterMode] = useState(null);
   const [currentSlide, setCurrentSlide] = useState(0);
   const [jump, setJump] = useState(false);
+  const [dragStart, setDragStart] = useState(null);
 
   useLayoutEffect(() => {
     const browserWidth = window.innerWidth;
@@ -35,6 +36,7 @@ const MainBanner = () => {
         setCurrentSlide(slideCount - 1);
       }
     };
+
     slideListRef.current.addEventListener('transitionend', onTransitionEnd);
 
     return () =>
@@ -53,6 +55,43 @@ const MainBanner = () => {
     setCurrentSlide(prev => prev + amount);
     setJump(false);
   };
+
+  useEffect(() => {
+    const startSwipe = event => {
+      slideListRef.current.style.transition = 'none';
+      setDragStart(event.clientX);
+    };
+
+    const doSwipe = event => {
+      if (dragStart === null) return;
+
+      slideListRef.current.style.transform = `translateX(-${
+        (slideCount + currentSlide) * 1084 - (event.clientX - dragStart)
+      }px)`;
+    };
+
+    const stopSwipe = event => {
+      if (dragStart === null) return;
+
+      slideListRef.current.style.transition = 'all 350ms ease-in-out';
+      if (dragStart > event.clientX) changeCurrent(1);
+      if (dragStart < event.clientX) changeCurrent(-1);
+
+      setDragStart(null);
+    };
+
+    slideListRef.current.addEventListener('mousedown', startSwipe);
+    slideListRef.current.addEventListener('mousemove', doSwipe);
+    slideListRef.current.addEventListener('mouseleave', stopSwipe);
+    slideListRef.current.addEventListener('mouseup', stopSwipe);
+
+    return () => {
+      slideListRef.current.removeEventListener('mousedown', startSwipe);
+      slideListRef.current.removeEventListener('mousemove', doSwipe);
+      slideListRef.current.removeEventListener('mouseleave', stopSwipe);
+      slideListRef.current.removeEventListener('mouseup', stopSwipe);
+    };
+  }, [dragStart]);
 
   return (
     <main className="Main">
